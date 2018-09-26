e3ffb6d226a0d3246613643396e6727c0cae64d1
Jiachen Yang <farseerfc@gmail.com>
2018-09-26 11:59:51 +0900

    fix gpd-power

--- gpd-power/gpd-power.py
+++ gpd-power/gpd-power.py
@@ -3,6 +3,10 @@ import sys
 from collections import namedtuple
 
 commands = dict()
+POWER_SUPPLY = "/sys/class/power_supply/"
+BATTARY = POWER_SUPPLY + "max170xx_battery/"
+USBC = POWER_SUPPLY + "tcpm-source-psy-i2c-fusb302/"
+CHARGER = POWER_SUPPLY + "bq24190-charger/"
 
 Command = namedtuple('Command', ['name', 'func', 'desc', 'dtype', 'rangemin', 'rangemax'])
 
@@ -23,40 +27,51 @@ def cat(path):
 
 @command('battary voltage now', 'float', 0, 15.0)
 def bat__voltage():
-    return float(cat('/sys/class/power_supply/max170xx_battery/voltage_now'))/1000000.0
-
+    return float(cat(BATTARY + 'voltage_now'))/1000000.0
 
 @command('battary current now', 'float', -5.0, 5.0)
 def bat__current():
-    return float(cat('/sys/class/power_supply/max170xx_battery/current_now'))/1000000.0
+    return float(cat(BATTARY + 'current_now'))/1000000.0
+
+@command('battary power (W)', 'float', -25.0, 25.0)
+def bat__power():
+    return bat__current() * bat__voltage()
 
 @command('usb-c source voltage now', 'float', 0, 15.0)
 def source__voltage():
-    return float(cat('/sys/class/power_supply/fusb302-typec-source/voltage_now'))/1000000.0
+    return float(cat(USBC + 'voltage_now'))/1000000.0
 
 @command('usb-c source current max', 'float', 0, 5.0)
 def source__current():
-    return float(cat('/sys/class/power_supply/fusb302-typec-source/current_max'))/1000000.0
+    return float(cat(USBC + 'current_max'))/1000000.0
 
 @command('usb-c power (W)', 'float', -25.0, 25.0)
 def source__power():
     return source__current()*source__voltage()
 
-@command('battary power (W)', 'float', -25.0, 25.0)
-def bat__power():
-    return bat__current()*bat__voltage()
+@command('charger voltage now', 'float', 0, 15.0)
+def charger__voltage():
+    return float(cat(CHARGER + 'constant_charge_voltage'))/1000000.0
+
+@command('charger current max', 'float', 0, 5.0)
+def charger__current():
+    return float(cat(CHARGER + 'constant_charge_current'))/1000000.0
+
+@command('charger power (W)', 'float', -25.0, 25.0)
+def charger__power():
+    return charger__current() * charger__voltage()
 
 @command('charge full', 'float', 0, 30)
 def charge__full():
-    return float(cat('/sys/class/power_supply/max170xx_battery/charge_full'))*3.5/1000000
+    return float(cat(BATTARY + 'charge_full'))*3.5/1000000
 
 @command('charge full design', 'float', 0, 30)
 def charge__full_design():
-    return float(cat('/sys/class/power_supply/max170xx_battery/charge_full_design'))*3.5/1000000
+    return float(cat(BATTARY + 'charge_full_design'))*3.5/1000000
 
 @command('charge now', 'float', 0, 30)
 def charge__now():
-    return float(cat('/sys/class/power_supply/max170xx_battery/charge_now'))*3.5/1000000
+    return float(cat(BATTARY + 'charge_now'))*3.5/1000000
 
 def main():
     print("ksysguardd 1.2.0")
