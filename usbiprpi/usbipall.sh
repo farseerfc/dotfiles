5b28c40186d74107f518519d1f957ea2fc22b55a
Jiachen YANG <farseerfc@gmail.com>
2019-02-20 16:09:21 +0900

    usbiprpi pass shellcheck

--- usbiprpi/usbipall.sh
+++ usbiprpi/usbipall.sh
@@ -1,15 +1,15 @@
-#/bin/sh
+#!/bin/sh
 (
 allusb=$(usbip list -p -l)
 for usb in $allusb
 do
-	busid=$(echo $usb | sed "s|#.*||g;s|busid=||g")
-	if [[ $busid = "1-1.1" ]]
+	busid=$(echo "$usb" | sed "s|#.*||g;s|busid=||g")
+	if [ "$busid" = "1-1.1" ]
 	then
 		# ignoring usb ethernet
 		continue
 	fi
 	echo "$(date -Iseconds): Exporting $busid"
-	usbip bind --busid=$busid
+	usbip bind --busid="$busid"
 done
 ) >>/var/log/usbipall.log 2>&1 
--- usbiprpi/usbiprpi3.sh
+++ usbiprpi/usbiprpi3.sh
@@ -1,16 +1,16 @@
-#/bin/sh
-rpi3="192.168.0.117"
+#!/bin/sh
+rpi3="192.168.0.21"
 
 modprobe vhci-hcd
 
 allusb=$(usbip list -p -r $rpi3 | cut -d":" -f1 -s | sed 's|^[ \t]*||;/^$/d')
 for busid in $allusb
 do
-	if [[ $busid = "1-1.1" ]]
+	if [ "$busid" = "1-1.1" ]
 	then
 		# ignoring usb ethernet
 		continue
 	fi
 	echo "Attaching $busid"
-	usbip attach --remote=$rpi3 --busid=$busid
+	usbip attach --remote=$rpi3 --busid="$busid"
 done
