050471513ddeaf3bfdc12661982e906b0a26d3e6
Jiachen YANG <farseerfc@gmail.com>
2020-06-12 11:11:28 +0900

    filesizehistogram.py: use format str and support multiple input filenames

--- zsh/.local/bin/filesizehistogram.py
+++ zsh/.local/bin/filesizehistogram.py
@@ -25,13 +25,11 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: filesizehistogram.py input')
         print('       can use "-" as input filename, indicate input is taken from stdin.')
-        print('       otherwise input file should be a result of "find -printf \'%s %p\'"')
+        print('       otherwise input file should be a result of "find -printf \'%s %p\\n\'"')
         sys.exit(1)
 
-    filename = sys.argv[1]
-    if filename == '-':
-       filename = '/dev/stdin'
-    data=np.array([int(x.split(' ')[0]) for x in open(filename)])
+    filenames = [x if x != '-' else '/dev/stdin' for x in sys.argv[1:]]
+    data=np.array([int(x.split(' ')[0]) for fn in filenames for x in open(fn)])
     mindatalog2 = 5 # cut from 32
     maxdatalog2 = min(ceil(log2(data.max())), 31) # cut at 1G and above
     # bins [0, 1, 32, 64, 128, 256, ... , 1G, 2G] , last bin is open range
@@ -47,20 +45,21 @@ if __name__ == '__main__':
     hist, bin_edges = np.histogram(data,bins)
     fig,ax = plt.subplots()
     ax.bar(range(len(hist)), hist, width=0.9)
-    ax.set_xticks([i for i,j in enumerate(hist)])
-    ax.set_xticklabels(['{}{}~{}'.format('┊\n'*(i%3), numfmt(bins[i]), numfmt(bins[i+1])) for i in range(len(hist)-1)] +
-                       ["{}~".format(numfmt(bins[len(hist)-1]))])
+    ax.set_xticks([i for i in range(len(hist))])
+    tickbar = "┊\n"
+    ax.set_xticklabels([f'{tickbar*(i%3)}{numfmt(bins[i])}~{numfmt(bins[i+1])}' for i in range(len(hist)-1)] +
+                       [f"{numfmt(bins[len(hist)-1])}~"])
 
     ax.axvline(bmean, color='k', linestyle='dashed', linewidth=1)
     ax.axvline(bmedian, color='r', linestyle='dashed', linewidth=2)
     min_ylim, max_ylim = plt.ylim()
     min_xlim, max_xlim = plt.xlim()
-    ax.text(bmean+0.5, max_ylim*0.9, 'Mean: {}'.format(numfmt(mean)))
-    ax.text(bmedian+0.5, max_ylim*0.9, 'Median: {}'.format(numfmt(median)), color='r')
-    ax.text(max_xlim*0.8, max_ylim*0.9, 'Files: {}'.format(files))
-    ax.text(max_xlim*0.9, max_ylim*0.9, 'Total: {}'.format(numfmt(float(total))))
+    ax.text(bmean + 0.5   , max_ylim * 0.9, f'Mean: {numfmt(mean)}')
+    ax.text(bmedian + 0.5 , max_ylim * 0.9, f'Median: {numfmt(median)}', color='r')
+    ax.text(max_xlim * 0.8, max_ylim * 0.9, f'Files: {files}')
+    ax.text(max_xlim * 0.9, max_ylim * 0.9, f'Total: {numfmt(float(total))}')
 
     for i in range(len(hist)):
-        ax.text(i-0.5, hist[i] + files/400, f"{hist[i]:5}") # label on top of every bar
+        ax.text(i - 0.5, hist[i] + files / 400, f"{hist[i]:5}") # label on top of every bar, uplefted a little
 
     plt.show()
